3311f3e53f419dcc042f2930af6a3c022ed03733
Documenter.jl <documenter@juliadocs.github.io>
2021-11-09 17:45:08 +0000

    build based on 329ea6d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CG1D","category":"page"},{"location":"#CG1D","page":"Home","title":"CG1D","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CG1D.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CG1D]","category":"page"}]
+}
